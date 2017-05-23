46d811e372c0839f638bf023e11b40af5ba16ef7
Priyesh Tungare <priyeshtungare@Priyeshs-MacBook-Pro.local>
2017-05-23 21:26:06 +0800

    merge feature-lifting-state-up into develop

--- src/game/board.js
+++ src/game/board.js
@@ -3,8 +3,34 @@ import Header from '../header/header';
 import Square from './square';
 
 class Board extends React.Component {
+  //When you want to aggregate data from multiple children or to have the child components
+  //communicate with each other, move the state upwards so that it lives in the parent
+  //component. The parent can then pass the state back down to children via props, so that
+  //child components are always in sync with each other and with the parent.
+
+  constructor() {
+      //In JS Classes, you need to explicitly call super(); when defining
+      //the constructor of a subclass.
+      super();
+      this.state = {
+          squares: Array(9).fill(null)
+      }
+  }
+
+  handleClick(i) {
+      //slice() is used to copy the squares array instead of mutating it.
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    //parenthesis are added so that JS doesn't insert semicolon after return.
+    //We are passing two props to Square, viz value and onClick.
+    return (<Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+    />);
   }
 
   render() {
--- src/game/square.js
+++ src/game/square.js
@@ -4,19 +4,10 @@ class Square extends React.Component {
 
   //React Components can have state by setting 'this.state' in the constructor,
   //which should be considered private to the component.
-  constructor(){
-      //In JS Classes, you need to explicitly call super(); when defining
-      //the constructor of a subclass.
-      super(); 
-      this.state = {
-          value: null
-      };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
