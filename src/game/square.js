1230292bfd10876709f3564cb37033ebeda50a3d
Priyesh Tungare <priyeshtungare@Priyeshs-MacBook-Pro.local>
2017-05-23 20:27:37 +0800

    added state to the component

--- src/game/square.js
+++ src/game/square.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 class Square extends React.Component {
+
+  //React Components can have state by setting 'this.state' in the constructor,
+  //which should be considered private to the component.
+  constructor(){
+      //In JS Classes, you need to explicitly call super(); when defining
+      //the constructor of a subclass.
+      super(); 
+      this.state = {
+          value: null
+      };
+  }
+
   render() {
     return (
-      <button className="square" onClick={() => alert('clicked')}>
-        {this.props.value}
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
       </button>
     );
   }
